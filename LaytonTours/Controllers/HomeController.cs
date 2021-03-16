308aba844afcd6c22b96ccb2d99615d8c22b16c1
Briton Demars <bdemars@byu.edu>
2021-03-16 11:47:44 -0600

    Beginning of Project 1 for 413

---
+++ LaytonTours/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTours.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAppointments()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
